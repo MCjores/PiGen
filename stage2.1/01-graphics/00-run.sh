88a4d706c658103922a600a9220d06a20eaa50e9
Андрей Бычков <andrew-by@hotmail.com>
2018-04-06 15:09:30 +0300

    Добавлена русская раскладка для экранной клавиатуры

--- stage2.1/01-graphics/00-run.sh
+++ stage2.1/01-graphics/00-run.sh
@@ -5,3 +5,5 @@ install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/pi/.config/openbox"
 touch "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
 chown 1000:1000 "${ROOTFS_DIR}/home/pi/.config/openbox/autostart" -v
 chmod +x "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
+
+echo "setxkbmap -layout us,ru -variant -option grp:lwin_toggle &" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
--- stage2.1/04-keyboard/00-run.sh
+++ stage2.1/04-keyboard/00-run.sh
@@ -2,12 +2,19 @@
 
 echo "onboard &" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
 
+install -m 644 files/Amplituda.onboard "${ROOTFS_DIR}/usr/share/onboard/layouts/"
+install -m 644 files/Amplituda-Alpha.svg "${ROOTFS_DIR}/usr/share/onboard/layouts/"
+install -m 644 files/Amplituda-Emoji.svg "${ROOTFS_DIR}/usr/share/onboard/layouts/"
+install -m 644 files/Amplituda-Numbers.svg "${ROOTFS_DIR}/usr/share/onboard/layouts/"
+install -m 644 files/Amplituda-Syms.svg "${ROOTFS_DIR}/usr/share/onboard/layouts/"
+install -m 644 files/globe.svg "${ROOTFS_DIR}/usr/share/onboard/layouts/images/"
+
 on_chroot << EOF
 su pi
 . <(dbus-launch --sh-syntax)
 
 gsettings set org.onboard system-theme-tracking-enabled false
-gsettings set org.onboard layout "/usr/share/onboard/layouts/Phone.onboard"
+gsettings set org.onboard layout "/usr/share/onboard/layouts/Amplituda.onboard"
 gsettings set org.onboard theme "/usr/share/onboard/themes/Nightshade.theme"
 
 gsettings set org.onboard.auto-show enabled true
