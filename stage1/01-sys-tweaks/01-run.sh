63cb73f390c452a9f45fb66b96d29b30e50d6aad
Андрей Бычков <andrew-by@hotmail.com>
2018-11-07 15:23:21 +0300

    Добавлено расширение файловой системы для раздела /home

--- stage1/01-sys-tweaks/01-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -e
-
-echo "HOMEDEV  /home           btrfs   rw,user,auto,exec,subvol=@home 0       0" >> "${ROOTFS_DIR}/etc/fstab"
--- stage2.2/00-mono/files/install_once.sh
+++ stage2.2/00-mono/files/install_once.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 if [ -d "/usr/lib/install-once/packages" ]; then
     plymouth message --text="Подготовка системы к первому использованию..."
+    mount -o remount,rw /
     cd /usr/lib/install-once/packages
     dpkg -i *.deb
     systemctl disable install-once
+    reboot
 fi
