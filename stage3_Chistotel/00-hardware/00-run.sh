91d77bf6725fda1b91b5217f1c063c3268a1c1c7
Андрей Бычков <andrew-by@hotmail.com>
2019-02-14 10:32:17 +0300

    Улучшен алгоритм включения UART на Чистотеле

--- stage3_Chistotel/00-hardware/00-run.sh
+++ stage3_Chistotel/00-hardware/00-run.sh
@@ -5,6 +5,8 @@ dtoverlay=i2c-rtc,ds3231
 enable_uart=1
 EOF
 
+sed -i "s/console=serial0,[0-9]* //" "${ROOTFS_DIR}/boot/cmdline.txt"
+
 on_chroot << EOF
 apt-get -y remove fake-hwclock
 update-rc.d -f fake-hwclock remove
