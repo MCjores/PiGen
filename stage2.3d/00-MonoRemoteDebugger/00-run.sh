6306c513539bd2ceb5b5bd0d3ecc9f38f0b08351
Андрей Бычков <andrew-by@hotmail.com>
2019-02-19 17:53:08 +0300

    Отладчик перенесён в /home

--- stage2.3d/00-MonoRemoteDebugger/00-run.sh
+++ stage2.3d/00-MonoRemoteDebugger/00-run.sh
@@ -5,8 +5,14 @@ systemctl enable ssh
 EOF
 
 wget https://github.com/techl/MonoRemoteDebugger/releases/download/v1.3.0/MonoRemoteDebugger.Server.zip
-unzip -d "${ROOTFS_DIR}/usr/lib/MonoRemoteDebugger.Server/" MonoRemoteDebugger.Server.zip
-chown 1000:1000 "${ROOTFS_DIR}/usr/lib/MonoRemoteDebugger.Server/" -Rv
-install -m 755 files/mono-remote-debugger "${ROOTFS_DIR}/usr/bin/"
+unzip -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/MonoRemoteDebugger.Server/" MonoRemoteDebugger.Server.zip
+chown 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/MonoRemoteDebugger.Server/" -Rv
+cat << EOF >> "${ROOTFS_DIR}/usr/bin/mono-remote-debugger"
+#!/bin/sh
 
-echo "mono-remote-debugger &" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
+exec /usr/bin/mono /home/${FIRST_USER_NAME}/MonoRemoteDebugger.Server/MonoRemoteDebugger.Server.exe "$@"
+EOF
+
+chmod +x "${ROOTFS_DIR}/usr/bin/mono-remote-debugger"
+
+echo "mono-remote-debugger &" >> "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/autostart"
