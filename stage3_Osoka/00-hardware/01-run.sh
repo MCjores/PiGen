0ac3b40fceeb1b9791e176b85557b5e14247c04c
Андрей Бычков <andrew-by@hotmail.com>
2020-04-22 17:11:03 +0300

    В Осоку добавлена конфигурация UART

--- stage3_Osoka/00-hardware/01-run.sh
+++ stage3_Osoka/00-hardware/01-run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
 sed -i -z "s/\n/ usbhid.mousepoll=0\n/" "${ROOTFS_DIR}/boot/cmdline.txt"
+sed -i "s/console=serial0,[0-9]* //" "${ROOTFS_DIR}/boot/cmdline.txt"
 install -m 644 files/asound.conf "${ROOTFS_DIR}/etc/"
 install -m 644 files/99-hidraw.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
 
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
 dtoverlay=i2c-rtc,ds3231
+enable_uart=1
 EOF
 
 on_chroot << EOF
