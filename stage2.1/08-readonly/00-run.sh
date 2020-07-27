6e153163c0cc0175254a1a1e174d8059c1175cd3
Андрей Бычков <andrew-by@hotmail.com>
2020-07-27 09:12:08 +0000

    Исправлены символьные ссылки

--- stage2.1/08-readonly/00-run.sh
+++ stage2.1/08-readonly/00-run.sh
@@ -10,15 +10,15 @@ sed -i -z "s/\n/ noswap ro\n/" "${ROOTFS_DIR}/boot/cmdline.txt"
 
 # Перемещение файла c настройками DNS на tmpfs
 mv "${ROOTFS_DIR}/etc/resolv.conf" "${ROOTFS_DIR}/tmp/dhcpcd.resolv.conf"
-ln -s "${ROOTFS_DIR}/tmp/dhcpcd.resolv.conf" "${ROOTFS_DIR}/etc/resolv.conf"
+ln -s "/tmp/dhcpcd.resolv.conf" "${ROOTFS_DIR}/etc/resolv.conf"
 
 # Размещение файла настройки аппаратных часов
 touch "${ROOTFS_DIR}/tmp/adjtime"
-ln -s "${ROOTFS_DIR}/tmp/adjtime" "${ROOTFS_DIR}/etc/adjtime"
+ln -s "/tmp/adjtime" "${ROOTFS_DIR}/etc/adjtime"
 
 # Перемещение зерна для генератора псевдослучайных чисел
 rm "${ROOTFS_DIR}/var/lib/systemd/random-seed" && \
-  ln -s "${ROOTFS_DIR}/tmp/random-seed" "${ROOTFS_DIR}/var/lib/systemd/random-seed"
+ln -s "/tmp/random-seed" "${ROOTFS_DIR}/var/lib/systemd/random-seed"
 install -m 644 files/systemd-random-seed.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 # Создание директории для файлов sudo time stamp
@@ -26,7 +26,7 @@ install -d "${ROOTFS_DIR}/var/lib/sudo/ts"
 
 # Создание директории для настроек звука
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/alsa"
-ln -s "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/alsa" "${ROOTFS_DIR}/var/lib/alsa"
+ln -s "/home/${FIRST_USER_NAME}/.config/alsa" "${ROOTFS_DIR}/var/lib/alsa"
 
 # Изменение таблицы разделов
 sed -i "/boot/ s/defaults/defaults,ro/g" "${ROOTFS_DIR}/etc/fstab"
