975f678ae1db9d0105f041d56102f9ee4c6012d0
Андрей Бычков <andrew-by@hotmail.com>
2021-01-14 15:57:01 +0300

    Исправлена сборка Чистотела

--- stage3_Chistotel/04-settings/00-run.sh
+++ stage3_Chistotel/04-settings/00-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Celandine"
-install -v -o 1000 -g 1000 -m 644 files/config/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Celandine/"
+#install -v -o 1000 -g 1000 -m 644 files/config/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Celandine/"
 install -v -o 1 -g 1 -m 644 files/celandine_ctr.spc "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
