57dd52568baa89d211277b999afa2232539a2b71
Андрей Бычков <andrew-by@hotmail.com>
2021-01-20 10:31:08 +0300

    Добавлены заводские настройки для Дозкалибратора

--- stage3_Dosecalibrator/03-software/00-run.sh
+++ stage3_Dosecalibrator/03-software/00-run.sh
@@ -3,4 +3,7 @@
 unzip files/Dosecal.zip -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}"
 chown -R 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Dosecal"
 
+install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Dosecalibrator"
+install -o 1000 -g 1000 files/Settings.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/Dosecalibrator/"
+
 echo "while true; do mono /home/${FIRST_USER_NAME}/Dosecal/Amplituda.RaspberryPi.Dosecalibrator.exe; sleep 1; done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
