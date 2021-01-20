fc13d65c1dde241889e806bb011fed784e3dcee5
Андрей Бычков <andrew-by@hotmail.com>
2021-01-20 10:31:23 +0300

    Добавлена программа калибровки дисплея

--- stage3_Dosecalibrator/01-display/00-run.sh
+++ stage3_Dosecalibrator/01-display/00-run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -e
 
+install -m 755          files/calibrate "${ROOTFS_DIR}/usr/sbin/"
+install -o 1000 -g 1000 files/rc.xml    "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/"
+
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
 max_usb_current=1
 hdmi_group=2
