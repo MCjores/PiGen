a8f20841876e3eedc4dbb325d0facb55ceafd5af
Андрей Бычков <andrew-by@hotmail.com>
2021-01-25 13:24:00 +0300

    Установка mono во время сборки системы

--- stage2.2/00-mono/files/install_once.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ -d "/usr/lib/install-once/packages" ]; then
-    plymouth message --text="Установка программ..."
-    cd /usr/lib/install-once/packages
-    dpkg -i *.deb
-    systemctl disable install-once
-fi
