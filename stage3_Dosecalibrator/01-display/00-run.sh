24397792fad99afa85a64ccb722b8975334788de
Aleksey Kornev <k0rallmail@gmail.com>
2021-04-14 17:36:14 +0300

    Исправление краха mono при запуске программы Добавлен скрипт копирования результатов калибровки тачскрина

--- stage3_Dosecalibrator/01-display/00-run.sh
+++ stage3_Dosecalibrator/01-display/00-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -m 755          files/calibrate "${ROOTFS_DIR}/usr/sbin/"
+install -m 755          files/calibrate_copy "${ROOTFS_DIR}/usr/sbin/"
 install -o 1000 -g 1000 files/rc.xml    "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/"
 
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
@@ -8,4 +8,5 @@ max_usb_current=1
 hdmi_group=2
 hdmi_mode=87
 hdmi_cvt=800 480 60 6 0 0 0
+display_rotate=2
 EOF
