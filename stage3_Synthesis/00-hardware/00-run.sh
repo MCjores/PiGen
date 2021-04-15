771a89b0840dda7ffd05377942c202f8ab08d843
Aleksey Kornev <k0rallmail@gmail.com>
2021-04-15 09:25:27 +0300

    Модуль синтеза переведён на Raspberry Pi 3B+ Во внешнем контроллере Ethernet более не нуждается

--- stage3_Synthesis/00-hardware/00-run.sh
+++ stage3_Synthesis/00-hardware/00-run.sh
@@ -3,13 +3,4 @@
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
 dtoverlay=i2c-rtc,ds3231
 enable_uart=1
-dtparam=spi=on
-dtoverlay=enc28j60
-EOF
-
-on_chroot << EOF
-su pi
-. <(dbus-launch --sh-syntax)
-timedatectl set-ntp false
-kill "\$DBUS_SESSION_BUS_PID"
 EOF
