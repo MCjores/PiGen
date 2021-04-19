7eb04db1a005d694ef9ef04a85768caa3d2ef2d4
Aleksey Kornev <k0rallmail@gmail.com>
2021-04-19 15:02:54 +0300

    Отладка конфигуратора

--- stage3_Dosecalibrator/04-configurator/00-run.sh
+++ stage3_Dosecalibrator/04-configurator/00-run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash -e
 
 install -m 755          files/firstboot "${ROOTFS_DIR}/usr/sbin/"
-install -m 755          files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 644          files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 install -v -o 1000 -g 1000 files/configurate.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
 
-mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator"
-mkdir "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/steps"
-mkdir "${ROOTFS_DIR}/var/lib/dosecalibrator"
+mkdir -p "${ROOTFS_DIR}/var/lib/dosecalibrator"
 touch "${ROOTFS_DIR}/var/lib/dosecalibrator/firstboot"
+on_chroot << EOF
 systemctl enable firstboot.service
+EOF
 
-install -v -o 1000 -g 1000 files/01-run.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/steps/"
+install -v -D -o 1000 -g 1000 files/00-step.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/steps/"
 
-sed -i '1s/^/~\/configurate.sh \n/' "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/autostart"
+sed -i '1s/^/~\/configurate.sh \& \n/' "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/autostart"
