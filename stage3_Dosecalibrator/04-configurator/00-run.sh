400eac92d6c8912b2a0692cbbec5d7abe1f18ab5
Aleksey Kornev <k0rallmail@gmail.com>
2021-04-19 16:47:41 +0300

    Конечная версия скрипта первичного запуска

--- stage3_Dosecalibrator/04-configurator/00-run.sh
+++ stage3_Dosecalibrator/04-configurator/00-run.sh
@@ -4,7 +4,8 @@ install -m 755          files/firstboot "${ROOTFS_DIR}/usr/sbin/"
 install -m 644          files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 install -v -o 1000 -g 1000 files/configurate.sh "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/"
-install -v -D -m 777 -o 1000 -g 1000 files/00-step.sh --target-directory="${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/steps/"
+install -v -D -o 1000 -g 1000 files/00-step.sh --target-directory="${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/steps/"
+chown -R 1000:1000 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/configurator/"
 
 sed -i '1s/^/~\/configurate.sh \& \n/' "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/.config/openbox/autostart"
 
