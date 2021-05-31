6d218a0490f5022b2ec2feb3baab8e7245b8fe3d
Aleksey Kornev <k0rallmail@gmail.com>
2021-05-31 17:16:04 +0300

    Запуск программы из нового места

--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -4,4 +4,4 @@ install -v -o 1000 -g 1000 files/* "${ROOTFS_DIR}/usr/lib/install-once/packages/
 
 dpkg -i files/amplituda-falconboi_1.2.0_linux-arm.deb
 
-echo "while true; do /usr/lib/amplituda-falcon-boi/Amplituda.Falcon.BOI /FullScreen; sleep 1; done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
+echo "while true; do ( cd /usr/lib/amplituda-falconboi/linux-arm &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
