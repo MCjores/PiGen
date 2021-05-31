3cbabe275493cdb9092d7242dcf77c311940f96a
Aleksey Kornev <k0rallmail@gmail.com>
2021-05-31 17:13:12 +0300

    Образ для БОИ Лиана-Химки

--- stage2.1/11-configure-apt/00-run.sh
+++ stage2.1/11-configure-apt/00-run.sh
@@ -2,8 +2,6 @@
 
 install -m 644 files/amplituda.list "${ROOTFS_DIR}/etc/apt/sources.list.d/"
 
-on_chroot update-mime-database ${ROOTFS_DIR}/usr/share/mime
-
 on_chroot apt-key add - < files/Amplituda.pubkey.gpg
 on_chroot << EOF
 apt-get update
--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -2,4 +2,6 @@
 
 install -v -o 1000 -g 1000 files/* "${ROOTFS_DIR}/usr/lib/install-once/packages/"
 
+dpkg -i files/amplituda-falconboi_1.2.0_linux-arm.deb
+
 echo "while true; do /usr/lib/amplituda-falcon-boi/Amplituda.Falcon.BOI /FullScreen; sleep 1; done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
