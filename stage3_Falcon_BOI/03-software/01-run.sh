74188c88fda4e7b59b1b5d7a27b03e134fb28900
Aleksey Kornev <k0rallmail@gmail.com>
2021-06-01 10:54:48 +0300

    Установка пакета БОИ

--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -e
 
-dpkg -i --force-architecture files/amplituda-falconboi_1.2.1_linux-arm.deb
+on_chroot << EOF
+dpkg -i files/amplituda-falconboi_1.2.1_linux-arm.deb
+EOF
 
 echo "while true; do ( cd /usr/lib/amplituda-falconboi/linux-arm &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
