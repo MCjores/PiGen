9e83e84bee3c24cd6526d283a55dcf62af2deabb
Aleksey Kornev <k0rallmail@gmail.com>
2021-06-01 10:28:03 +0300

    Отладка образа БОИ

--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-dpkg -i --force-architecture files/amplituda-falconboi_1.2.0_linux-arm.deb
+dpkg -i --force-architecture files/amplituda-falconboi_1.2.1_linux-arm.deb
 
-echo "while true; do ( cd /usr/lib/amplituda-falcon-boi &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
+echo "while true; do ( cd /usr/lib/amplituda-falconboi/linux-arm &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
