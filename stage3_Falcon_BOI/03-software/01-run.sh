1a9190ee7373667f076a1076c32e9d4ed66e9a8c
Aleksey Kornev <k0rallmail@gmail.com>
2021-06-01 11:02:13 +0300

    Удаление evdev Настройка установщика приложения БОИ

--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+install -m 755 -C          files/* "${ROOTFS_DIR}/home/pi/"
+
 on_chroot << EOF
-dpkg -i files/amplituda-falconboi_1.2.1_linux-arm.deb
+dpkg -i /home/pi/amplituda-falconboi_1.2.1_linux-arm.deb
 EOF
 
 echo "while true; do ( cd /usr/lib/amplituda-falconboi/linux-arm &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
