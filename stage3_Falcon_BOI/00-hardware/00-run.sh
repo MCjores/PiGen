c0d4c2ff45a1215b5aeb50200a0cd61cb95700a5
Aleksey Kornev <k0rallmail@gmail.com>
2021-06-02 18:16:11 +0300

    Добавлено уменьшение видимой области из-за физической рамки на экране

--- stage3_Falcon_BOI/00-hardware/00-run.sh
+++ stage3_Falcon_BOI/00-hardware/00-run.sh
@@ -5,6 +5,7 @@ install -m 644 files/99-hidraw.rules "${ROOTFS_DIR}/etc/udev/rules.d/99-hidraw.r
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
 dtoverlay=i2c-rtc,ds3231
 enable_uart=1
+overscan_left=16
 EOF
 
 sed -i "s/console=serial0,[0-9]* //" "${ROOTFS_DIR}/boot/cmdline.txt"
