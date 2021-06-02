36270aa6bd6036a8c21e40c03f16461bbe5cadb1
Aleksey Kornev <k0rallmail@gmail.com>
2021-06-02 17:40:25 +0300

    Новая версия с исправленной утечкой памяти

--- stage3_Falcon_BOI/03-software/01-run.sh
+++ stage3_Falcon_BOI/03-software/01-run.sh
@@ -3,7 +3,7 @@
 install -m 755 -C          files/* "${ROOTFS_DIR}/home/pi/"
 
 on_chroot << EOF
-dpkg -i /home/pi/amplituda-falconboi_1.2.1_linux-arm.deb
+dpkg -i /home/pi/amplituda-falconboi_1.2.2_linux-arm.deb
 EOF
 
 echo "while true; do ( cd /usr/lib/amplituda-falconboi/linux-arm &&  ./Amplituda.Falcon.BOI /FullScreen; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
