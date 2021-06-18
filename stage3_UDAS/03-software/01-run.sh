ed953595c4826a47f4491b567087c03d212ed498
Kornev Aleksey <kornev_aleksey@inbox.ru>
2021-06-18 14:53:34 +0300

    Добавлена установка libgee-0.8.2 для работы фронта Скорректирован скрипт установки ПО

--- stage3_UDAS/03-software/01-run.sh
+++ stage3_UDAS/03-software/01-run.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-dpkg -i /home/pi/amplituda-udas03_1.1.0_linux-arm.deb
-EOF
+mkdir -p "${ROOTFS_DIR}/home/pi/UDAS03"
+mkdir -p "${ROOTFS_DIR}/home/pi/distribs"
 
-install -m 755 -C -d          files/UDAS-03 "${ROOTFS_DIR}/home/pi/UDAS03"
-install -m 755 -C             files/udas03.service "${ROOTFS_DIR}/etc/systemd/system"
+install -m 755 -C            files/UDAS-03 "${ROOTFS_DIR}/home/pi/UDAS03/UDAS-03"
+install -m 755 -C            files/amplituda-udas03_1.1.0_linux-arm.deb "${ROOTFS_DIR}/home/pi/distribs/amplituda-udas03_1.1.0_linux-arm.deb"
+install -m 755 -C            files/udas03.service "${ROOTFS_DIR}/etc/systemd/system/udas03.service"
+
+on_chroot << EOF
+dpkg -i /home/pi/distribs/amplituda-udas03_1.1.0_linux-arm.deb
+EOF
 
 echo "while true; do ( ~/UDAS03/UDAS-03 -F; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
