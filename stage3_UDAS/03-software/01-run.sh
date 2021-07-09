3ef86e3a11b1e9c98ec6e4b31477a20350de2fe8
Aleksey Kornev <k0rallmail@gmail.com>
2021-07-09 17:50:38 +0300

    Добавлена конфигурация

--- stage3_UDAS/03-software/01-run.sh
+++ stage3_UDAS/03-software/01-run.sh
@@ -8,3 +8,9 @@ install -m 644 -C            files/*.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}
 install -m 755 -C            files/udas03.service "${ROOTFS_DIR}/etc/systemd/system/udas03.service"
 
 echo "while true; do ( ~/UDAS03/UDAS-03 -F; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
+
+mkdir -p "${ROOTFS_DIR}/home/mariadb/mysql"
+chown -R mysql:mysql "${ROOTFS_DIR}/home/mariadb"
+chmod -R 777 "${ROOTFS_DIR}/home/mariadb"
+install -m 755 -C            files/mariadb/50-server.conf "${ROOTFS_DIR}/etc/mysql/mariadb.conf.d/50-server.cnf"
+install -m 755 -C            files/mariadb/dontprotecthome.conf "${ROOTFS_DIR}/etc/systemd/system/mariadb.service.d/dontprotecthome.conf"
