e3ebda918956176f0085aa03f68aeb8e7503f008
Aleksey Kornev <k0rallmail@gmail.com>
2021-07-09 09:46:55 +0300

    Добавлены файлы конфигурации УДАС-03 Установка УДАС-03 через репозиторий

--- stage3_UDAS/01-display/01-run.sh
+++ stage3_UDAS/01-display/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 cat << EOF >> "${ROOTFS_DIR}/boot/config.txt"
-lcd_rotate=2
+lcd_rotate=0
 hdmi_force_hotplug=1
 EOF
--- stage3_UDAS/03-software/01-run.sh
+++ stage3_UDAS/03-software/01-run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash -e
 
-mkdir -p "${ROOTFS_DIR}/home/pi/UDAS03"
-mkdir -p "${ROOTFS_DIR}/home/pi/distribs"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/UDAS03/References"
 
-install -m 755 -C            files/UDAS-03 "${ROOTFS_DIR}/home/pi/UDAS03/UDAS-03"
-install -m 755 -C            files/amplituda-udas03_1.1.0_linux-arm.deb "${ROOTFS_DIR}/home/pi/distribs/amplituda-udas03_1.1.0_linux-arm.deb"
+install -m 755 -C            files/UDAS-03 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/UDAS03/UDAS-03"
+install -m 644 -C            files/References/* "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Amplituda/UDAS-03/References/"
+install -m 644 -C            files/*.json "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Amplituda/UDAS03/"
 install -m 755 -C            files/udas03.service "${ROOTFS_DIR}/etc/systemd/system/udas03.service"
-
-on_chroot << EOF
-dpkg -i /home/pi/distribs/amplituda-udas03_1.1.0_linux-arm.deb
-systemctl enable udas03.service
-EOF
 
 echo "while true; do ( ~/UDAS03/UDAS-03 -F; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
