802b1f27ecebe5af68e040e9eecd581fb3ff8912
Kornev Aleksey <kornev_aleksey@inbox.ru>
2021-07-27 16:10:45 +0300

    Изменения в скрипте конфигурации

--- stage3_UDAS/03-software/01-run.sh
+++ stage3_UDAS/03-software/01-run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
-mkdir -p -v "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Amplituda/UDAS-03/References"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Amplituda/UDAS-03/References"
+mkdir -p "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/UDAS03"
 
 install -m 755 -C            files/UDAS-03 "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/UDAS03/UDAS-03"
 install -v -m 644 -d         files/References "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/Amplituda/UDAS-03/References"
@@ -10,5 +11,6 @@ install -m 755 -C            files/udas03.service "${ROOTFS_DIR}/etc/systemd/sys
 echo "while true; do ( ~/UDAS03/UDAS-03 -F; sleep 1; ) done" >> "${ROOTFS_DIR}/home/pi/.config/openbox/autostart"
 
 mkdir -p "${ROOTFS_DIR}/home/mariadb/mysql"
-install -m 755 -C            files/mariadb/50-server.conf "${ROOTFS_DIR}/etc/mysql/mariadb.conf.d/50-server.cnf"
+install -m 755 -C            files/mariadb/50-server.cnf "${ROOTFS_DIR}/etc/mysql/mariadb.conf.d/50-server.cnf"
+mkdir -p "${ROOTFS_DIR}/etc/systemd/system/mariadb.service.d"
 install -m 755 -C            files/mariadb/dontprotecthome.conf "${ROOTFS_DIR}/etc/systemd/system/mariadb.service.d/dontprotecthome.conf"
