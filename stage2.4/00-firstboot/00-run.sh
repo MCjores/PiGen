9a2c463d95805200bf6edcf878b8610822445441
Aleksey Kornev <k0rallmail@gmail.com>
2021-07-27 17:26:35 +0300

    Исправлена загрузка сервиса УДАС-03

--- stage2.4/00-firstboot/00-run.sh
+++ stage2.4/00-firstboot/00-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 install -m 755          files/firstboot "${ROOTFS_DIR}/usr/sbin/"
-install -m 644       files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
+install -m 755       files/firstboot.service "${ROOTFS_DIR}/etc/systemd/system/"
 
 mkdir -p "${ROOTFS_DIR}/var/lib/amplituda"
 touch "${ROOTFS_DIR}/var/lib/amplituda/firstboot"
